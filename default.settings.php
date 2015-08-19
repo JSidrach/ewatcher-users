fc983e3608a9a568f765ea39c92bb3eaf9499815
JSidrach <juan.sidrach@gmail.com>
2015-08-19 11:05:45 +0200

    Quitar propiedades no necesarias

--- default.settings.php
+++ default.settings.php
@@ -20,7 +20,6 @@
     "P5" => array("type" => "tinyint(1)", "Null" => "NO", "default" => 0),
     "cIn" => array("type" => "float", "Null" => "NO", "default" => 0.1244),
     "cOut" => array("type" => "float", "Null" => "NO", "default" => 0.054),
-    "units" => array("type" => "char(8)", "Null" => "NO", "default" => "€"),
-    "config" => array("type" => "text")
+    "units" => array("type" => "char(8)", "Null" => "NO", "default" => "€")
   );
 ?>
